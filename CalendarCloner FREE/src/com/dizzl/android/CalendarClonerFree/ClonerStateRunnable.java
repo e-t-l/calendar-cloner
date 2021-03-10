c109a566812277ac8dc351fa3e5b2446bdbc71d4
e-t-l <40775958+e-t-l@users.noreply.github.com>
2021-03-09 20:01:29 -0500

    Delete CalendarCloner FREE directory

--- CalendarCloner FREE/src/com/dizzl/android/CalendarClonerFree/ClonerStateRunnable.java
+++
@@ -1,9 +0,0 @@
-package com.dizzl.android.CalendarClonerFree;
-
-public abstract class ClonerStateRunnable {
-	// Both constants are defined as negative numbers to not conflict with possible resync times
-	public static final long CLONER_RUNNING = -1;
-	public static final long CLONER_NOT_RUNNING = -2;
-
-	public abstract void run(long clonerStateOrResyncTime);
-}
